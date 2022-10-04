456dcc0d5f6dc660e47ffb6e241f65097ae08a4a
antondroid <win22n@yandex.ua>
2022-10-05 01:43:35 +0300

    the task is completed in .v1
    the task is completed in .v1
    .v0 has keep the various useful methods for my own reference

--- songs_v1.py
+++ songs_v1.py
@@ -1,13 +1,9 @@
 # Есть строка с перечислением песен
 
 my_favorite_songs = 'Waste a Moment, Staying\' Alive, A Sorta Fairytale, Start Me Up, New Salvation'
-output=my_favorite_songs.split(', ')
-length=len(output)
-print(length)
-print(output[:2])
-print(output[2])
-print(output[0],output[length-1],output[1],output[length-2])
-print(my_favorite_songs.split(', '))
+output=my_favorite_songs.split(', ')                            #split string to the list         
+length=len(output)                                              #count the  list cells 
+print(output[0],output[length-1],output[1],output[length-2])    #output to console by the changed order
 #----------------my comments------------------------
 #as it didn't prompted, how to output the tokens, i out it in line 
 # with one space only and without any separators
