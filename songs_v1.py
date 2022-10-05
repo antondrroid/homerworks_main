20a75d1cbbef6ba8698d201232cc9afeefdf95a2
Nikita Pyshkov <85805562+pyshkovni@users.noreply.github.com>
2022-10-05 22:23:59 +0300

    Добавил пример токенизации
    Описал пример токенизации и код для анализа повторений.

--- songs_v1.py
+++ songs_v1.py
@@ -13,3 +13,36 @@ print(output[0],output[length-1],output[1],output[length-2])    #output to conso
 # Выведите на консоль с помощью индексации строки, последовательно:
 #  первый трек, последний, второй, второй с конца
 # Нельзя переопределять my_favorite_songs и запятая не должна выводиться.
+
+
+######################################################
+# Токенизация и анализ количества слов
+
+# Импорт функций из пакетов pandas и scikit-learn
+# Для пакетного менеджера conda:
+#     conda install pandas
+#     conda install scikit-learn
+from pandas import DataFrame 
+from sklearn.feature_extraction.text import CountVectorizer
+
+# Коллекция из двух плейлистов с песнями
+my_collection = [
+    'Waste a Moment, Staying\' Alive, A Sorta Fairytale, Start Me Up, New Salvation', 
+    'World in My Eyes, Firestarter, Personal Jesus, Still Alive, New Soul'
+    ]
+
+# Таблица из номеров плейлистов и строки песен
+df = DataFrame({'collection': ['playlist_1', 'playlist_2'], 'names':my_collection})
+
+# CountVectorizer создает вектор токенов
+# параметр stop_words для исключения ненужных слов 
+cv = CountVectorizer(stop_words=['a', 'an', 'the', 'in']) 
+cv_matrix = cv.fit_transform(df['names'])  # преобразуем в набор данных
+
+# Создаем таблицу, преобразуя cv_matrix в массив
+df_dtm = DataFrame(cv_matrix.toarray(), index=df['collection'].values, columns=cv.get_feature_names_out())
+
+print(df_dtm.transpose())  # вывод и транспонирование
+
+#### Подробее про токенизацию по ссылке
+#### https://habr.com/ru/post/504744/
